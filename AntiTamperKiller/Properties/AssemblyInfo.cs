4061cd12cb1212c3017e97788703c430cd049f9d
文煌 <wwh1004@hotmail.com>
2018-08-14 18:13:34 +0800

    update

---
+++ AntiTamperKiller/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AntiTamperKiller")]
+[assembly: AssemblyProduct("AntiTamperKiller")]
+[assembly: AssemblyCopyright("Copyright © 2018 Wwh / NCK")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
