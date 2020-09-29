3705cd951ed2b9851580c191cb954d3fc6e5988a
文煌 <wwh1004@hotmail.com>
2020-09-29 23:26:14 +0800

    移除错误复制的设置

--- ConstantKiller/ConstantKillerSettings.cs
+++ ConstantKiller/ConstantKillerSettings.cs
@@ -5,28 +5,16 @@ using System.IO;
 namespace ConfuserExTools.ConstantKiller {
 	public sealed class ConstantKillerSettings {
 		private string _assemblyPath;
-		private bool _preserveProxyMethods;
 		private bool _preserveAll;
-		private bool _ignoreAccess;
 
 		[Argument("-f", IsRequired = true, Type = "FILE", Description = "程序集路径")]
 		internal string AssemblyPathCliSetter {
 			set => AssemblyPath = value;
 		}
 
-		[Argument("--preserve-proxies", Description = "是否保留代理方法")]
-		internal bool PreserveProxyMethodsCliSetter {
-			set => PreserveProxyMethods = value;
-		}
-
 		[Argument("--preserve-all", Description = "是否保留全部，仅还原代理方法")]
 		internal bool PreserveAllCliSetter {
 			set => PreserveAll = value;
-		}
-
-		[Argument("--ignore-access", Description = "是否忽略访问权限")]
-		internal bool IgnoreAccessCliSetter {
-			set => IgnoreAccess = value;
 		}
 
 		public string AssemblyPath {
@@ -41,19 +29,9 @@ namespace ConfuserExTools.ConstantKiller {
 			}
 		}
 
-		public bool PreserveProxyMethods {
-			get => _preserveProxyMethods;
-			set => _preserveProxyMethods = value;
-		}
-
 		public bool PreserveAll {
 			get => _preserveAll;
 			set => _preserveAll = value;
-		}
-
-		public bool IgnoreAccess {
-			get => _ignoreAccess;
-			set => _ignoreAccess = value;
 		}
 	}
 }
